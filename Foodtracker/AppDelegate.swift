c2a7b90021d4cfdc9bcadcf25371bf425f391d23
Bijay Kumar Pun <homay@Bijays-MacBook-Pro.local>
2020-05-20 23:31:40 +0545

    part 1: building a basic UI

--- Foodtracker/AppDelegate.swift
+++ Foodtracker/AppDelegate.swift
@@ -6,12 +6,26 @@
 //  Copyright © 2020 Home. All rights reserved.
 //
 
+/**
+ The AppDelegate.swift has two prmary functions:
+- Creates the window where the app's content is drawn
+- Careates entry point to the app, and run loop that delivers input events to the app, done by the @UIApplicationMain attribute
+ */
+
+
 import UIKit
 
 @UIApplicationMain
+// Using this attribute is equivalent to calling UIAPplicationMain function and passing
+// AppDelegate class's name as the name of the delegate class. In response, the system creates an application object, which is responsible
+// for managing the life cycle of the app
+// Link ; //https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/BuildABasicUI
+// .html#//apple_ref/doc/uid/TP40015214-CH5-SW1
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    // Stub implementation
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,7 +45,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+        //stub implementation
+    }
 
 
 }
+
+
 
--- Foodtracker/ViewController.swift
+++ Foodtracker/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+        
+        //method stub
     }
 
 
 }
+
 
