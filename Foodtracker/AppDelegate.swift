8830c1eec555cff9bcf80d4ef1d9fc850ce9314c
Bijay Kumar Pun <homay@Bijays-MacBook-Pro.local>
2020-05-21 09:59:46 +0545

    Part 2: wip: Connect UI to code

--- Foodtracker/AppDelegate.swift
+++ Foodtracker/AppDelegate.swift
@@ -46,10 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        
-        //stub implementation
-    }
+ 
 
 
 }
--- Foodtracker/ViewController.swift
+++ Foodtracker/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: properties
+    
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    //@IBOutlet atribute tells xocde that nameTextField property can be connected from the
+    // interface builder, which is why the IB prefix
+    //The weak keyword indicates that the reference does not prevent the system from deallocating the
+    // referenced object
+    // Rest define as : implicitly unwrapped optional variable of type UITextField named nameTextField
+    
+
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view.
@@ -20,7 +37,21 @@ class ViewController: UIViewController {
         
         //method stub
     }
-
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultTextLabel(_ sender: UIButton) {
+        // The sender parameter refers to the object responsible for triggering the action
+        //IBAction attribute indicates that the method is an action connected from the storyboard
+        // interface builder
+        // Rest declares method by the name of setDefaultLabelText(_:)
+        nameTextField.text = "Default Text";
+    }
+        
+   
+        
+    
+    
 
 }
 
