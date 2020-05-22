92c4e3c8f3ba0ce624eeb17ba8bcd5261a4318e8
Bijay Kumar Pun <homay@Bijays-MacBook-Pro.local>
2020-05-22 21:01:30 +0545

    part 4: wip: add rating star image assets

--- Foodtracker/RatingControl.swift
+++ Foodtracker/RatingControl.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-@objcMembers class RatingControl: UIStackView {
+ @IBDesignable @objcMembers class RatingControl: UIStackView {
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -20,7 +20,17 @@ import UIKit
     */
     //MARK: properties
     private var ratingButtons = [UIButton](); //property with list of buttons
-    var rating = 0; //controls' rating
+    var rating = 0; //controls' rating'
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet{
+            setupButtons(); //property observers
+        }    }
+    @IBInspectable var startCount: Int = 5{
+        didSet{
+            setupButtons(); //property observers
+        }
+    }
     
     //MARK: initilalization
     
@@ -38,15 +48,35 @@ import UIKit
     //MARK: private methods
     
     private func setupButtons(){
-        for _ in 0..<5{ //half-open range operator(..<) doesn't include the upper number, so this goes from 0 to 4, the underscore _ is  a
+        //clear any existing buttons; supposedly to update the view in IB
+        for button in ratingButtons{
+            removeArrangedSubview(button);
+            button.removeFromSuperview();
+        }
+        ratingButtons.removeAll();
+        
+        //load button images
+        //also load images from asset catalog
+        let bundle = Bundle(for: type(of: self));
+        let filledStar = UIImage(named:"filledStar", in: bundle, compatibleWith: self.traitCollection);
+        let emptyStar = UIImage(named:"emptyStar", in: bundle, compatibleWith: self.traitCollection);
+        let highlightedStar = UIImage(named:"highlightedStar", in: bundle, compatibleWith: self.traitCollection);
+        
+        
+        for _ in 0..<startCount{ //half-open range operator(..<) doesn't include the upper number, so this goes from 0 to 4, the underscore _ is  a
             // wildcard, which can be used when we don't need to know which iterations of the loop is currently executing
             let button = UIButton();
-            button.backgroundColor = UIColor.red;
+            
+            //button.backgroundColor = UIColor.red;
+            button.setImage(emptyStar, for: .normal);
+            button.setImage(filledStar, for: .selected);
+            button.setImage(highlightedStar, for: .highlighted);
+            button.setImage(highlightedStar, for: [.highlighted, .selected]);
             
             //Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false; //disable automatically generated constraint
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true; // add a constraint to button's height anchor
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true; // add a constraint to button's width anchor
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true; // add a constraint to button's height anchor
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true; // add a constraint to button's width anchor
             
             //setup the button action
             
